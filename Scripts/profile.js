c01df32dd334506eeabb1135f8f3a17432d4b3ba
Frigaa <omarfriga20@gamil.com>
2025-03-11 00:29:38 +0200

    Add Profile Page

---
+++ Scripts/profile.js
@@ -0,0 +1,25 @@
+function showFinishedBooking() {
+    document.getElementById("finishedBooking").style.display = "block";
+    document.getElementById("UpcomingBooking").style.display = "none";
+    setActiveTab("finished");
+  }
+  function showUpcomingBooking() {
+    document.getElementById("UpcomingBooking").style.display = "block";
+    document.getElementById("finishedBooking").style.display = "none";
+    setActiveTab("upcoming");
+  }
+  function setActiveTab(tab) {
+    document.querySelectorAll(".tabs ul li").forEach((li) => {
+      li.classList.remove("active");
+    });
+    document.querySelector(`.tabs ul li.${tab}`).classList.add("active");
+  }
+
+ 
+  document.querySelectorAll('.cancel-btn').forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Booking Cancelled');
+      const card = button.closest('.card'); 
+      card.remove(); 
+    });
+  });
