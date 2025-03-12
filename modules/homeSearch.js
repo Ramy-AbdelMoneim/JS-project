645b43d4a4b54a54b1fadb3cb5c75fbac302353b
John Rezaik <johnrezaik@gmail.com>
2025-03-13 01:59:51 +0200

    tested search

--- modules/homeSearch.js
+++ modules/homeSearch.js
@@ -1,6 +1,5 @@
 import { filterByCity, filterByDay, filterByHours } from "./filterBy.js";
 import { loadData } from "../modules/dataFunctions.js";
-import { getCurrentData, updateData } from "../Scripts/Home.js";
 function searchlisten(updateSliderFn) {
   document
     .getElementById("searchForm")
@@ -10,10 +9,8 @@ function searchlisten(updateSliderFn) {
 
   const searchBtn = document.getElementById("home-search-btn");
   searchBtn.addEventListener("click", async () => {
-    const data = await getCurrentData();
-    if (data.length === 0) {
-      data = await loadData();
-    }
+    const data = await loadData();
+
     const city = document.getElementById("search-city").value;
     let fromHour = parseInt(document.getElementById("search-from-hour").value);
     let toHour = parseInt(document.getElementById("search-to-hour").value);
@@ -21,13 +18,7 @@ function searchlisten(updateSliderFn) {
     for (let i = fromHour; i < toHour; i++) {
       hours.push(i);
     }
-
-    const result = filterByHours(
-      filterByDay(filterByCity(originalData, city)),
-      hours
-    );
-
-    updateData(result);
+    const result = filterByHours(filterByDay(filterByCity(data, city)), hours);
     updateSliderFn(result);
   });
 }
